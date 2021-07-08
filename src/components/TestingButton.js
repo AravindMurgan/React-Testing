7d9c5589bbab7f622024a48fa5188f393f2b00cc
Aravind Murugan <mi.arvindmurugan@gmail.com>
2021-07-08 16:25:50 +0530

    testingbutton

---
+++ src/components/TestingButton.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function TestingButton() {
+    return (
+        <div>
+            <button style={{backgroundColor:'red'}}  >Click to change to  </button>
+        </div>
+    )
+}
+
+export default TestingButton

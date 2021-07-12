3002bf04cdd7a1a6b07eda2664fbf712494663e9
Aravind Murugan <mi.arvindmurugan@gmail.com>
2021-07-12 15:42:16 +0530

    summary-testDone

--- src/pages/test/SummaryForm.test.js
+++ src/pages/test/SummaryForm.test.js
@@ -1,24 +1,29 @@
-import { fireEvent } from '@testing-library/react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import SummaryForm from '../summary/SummaryForm';
 
-describe('summary-form-testing', () => {
+test('initial conditions', () => {
 	render(<SummaryForm />);
 
-	test('checkbox and button', () => {
-		const checkbox = screen.getByRole('checkbox');
-		const button = screen.getByRole('button');
-        //checkbox default//
-        expect(checkbox).not.toBeChecked();
-
-		///click checkbox and enable button//
-        fireEvent.click(checkbox)
-        expect(button).toBeEnabled();
-
-        //uncheck and disable button//
-        fireEvent.click(checkbox);
-        expect(button).toBeDisabled();
+	const checkbox = screen.getByRole('checkbox', {
+		name: /terms and conditions/i,
 	});
+	const button = screen.getByRole('button', { name: /confirm order/i });
 
+	expect(checkbox).not.toBeChecked();
 
+	expect(button).toBeDisabled();
+});
+
+test('enable disable checkbox and button', () => {
+	render(<SummaryForm />);
+	const checkbox = screen.getByRole('checkbox', {
+		name: /terms and conditions/i,
+	});
+	const button = screen.getByRole('button', { name: /confirm order/i });
+    //enable checkbox and button//
+	fireEvent.click(checkbox);
+	expect(button).toBeEnabled();
+    ///disable checkbox and button//
+	fireEvent.click(checkbox);
+	expect(button).toBeDisabled();
 });
